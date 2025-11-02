c29e9986d9698cd84491900a03f36e269cf7d08b
Sam Fakhreddine <sam.fakhreddine@rackspace.com>
2025-11-02 09:10:43 -0700

    Add HTTP request/response logging with tower-http trace layer

--- radarr_api/src/main.rs
+++ radarr_api/src/main.rs
@@ -87,7 +87,16 @@ async fn main() -> anyhow::Result<()> {
 
     // Build router with state
     let app_state = routes::AppState::new(movie_service, config.clone());
-    let app = routes::movie_routes().with_state(app_state);
+    let app = routes::movie_routes()
+        .layer(
+            tower_http::trace::TraceLayer::new_for_http()
+                .make_span_with(tower_http::trace::DefaultMakeSpan::new().level(tracing::Level::INFO))
+                .on_request(tower_http::trace::DefaultOnRequest::new().level(tracing::Level::INFO))
+                .on_response(
+                    tower_http::trace::DefaultOnResponse::new().level(tracing::Level::INFO),
+                ),
+        )
+        .with_state(app_state);
     tracing::debug!("Router configured");
 
     // Start HTTP server
